2a157a1153b73bf7791b37eb05fda13fc6425526
Jatin Dua <jdua004@gmail.com>
2023-11-14 18:32:29 +0530

    feat: Add gorilla websockets

--- server/main.go
+++ server/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+    "github.com/gofiber/contrib/websocket"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
@@ -12,6 +13,40 @@ func setupRoutes() {
 
 	app.Use(logger.New(logger.Config{
 		Format: "[${ip}]:${port} ${status} - ${method} ${path}\n",
+	}))
+
+	app.Use("/ws", func(c *fiber.Ctx) error {
+		if websocket.IsWebSocketUpgrade(c) {
+			c.Locals("allowed", true)
+			return c.Next()
+		}
+
+		return fiber.ErrUpgradeRequired
+	})
+
+	app.Get("/ws", websocket.New(func(c *websocket.Conn) {
+		// c.Locals is added to the *websocket.Conn
+	
+		var (
+			mt int
+			msg []byte
+			err error
+		)
+
+		for {
+			if mt, msg, err = c.ReadMessage(); err != nil {
+				log.Println("read: ", err)
+				break
+			}
+
+			log.Printf("recv: %s", msg)
+
+			if err = c.WriteMessage(mt, msg); err != nil {
+				log.Println("write: ", err)
+				break
+			}
+		}
+
 	}))
 
 	app.Get("/", func(c *fiber.Ctx) error {
