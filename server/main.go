0943763b9f8538d8c9083de7c32bb4b1511d8c44
Jatin Dua <jdua004@gmail.com>
2023-11-14 18:28:03 +0530

    feat: Add request logger

--- server/main.go
+++ server/main.go
@@ -4,10 +4,15 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func setupRoutes() {
 	app := fiber.New()
+
+	app.Use(logger.New(logger.Config{
+		Format: "[${ip}]:${port} ${status} - ${method} ${path}\n",
+	}))
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello from Fiber!")
